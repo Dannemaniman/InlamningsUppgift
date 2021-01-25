610c3b26b21fab0deae7477a5451573c422535b6
Dannemaniman <daniellangas91@gmail.com>
2021-01-25 17:28:54 +0100

    Added Logic for Game class and Main

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,4 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Game {
+    public static void main(String[] args) {
+       newGame();
+    }
+
+    public static void newGame(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--- Welcome to the Game! ---\n");
+        System.out.println(" - Hur många rundor vill du köra? - \n");
+        int rundor = Integer.parseInt(scanner.nextLine());
+    }
 }
